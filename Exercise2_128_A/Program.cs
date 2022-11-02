17793d00bd57b26711e8e4e645172cf8ce0babdd
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-11-02 16:30:11 +0700

    memasukkan int pada data

--- Exercise2_128_A/Program.cs
+++ Exercise2_128_A/Program.cs
@@ -21,6 +21,17 @@ namespace Exercise2
                     Console.WriteLine("\nData dapat memiliki maksimal 7 elemen.\n");
 
             }
+            Console.WriteLine("");
+            Console.WriteLine("==============================================================");
+            Console.WriteLine(" Masukkan jumlah elemen data ");
+            Console.WriteLine("==============================================================");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
         }
     }
 }
